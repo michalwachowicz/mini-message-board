0e84ac8408253eedb55763893a7391da02e40598
michalwachowicz <wachowiczmichal1@gmail.com>
2025-06-18 12:20:56 +0200

    feat: use db connection string in .env

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,13 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export function getConnection() {
-  const url = process.env.PGHOST || "localhost";
-  const username = process.env.PGUSER || "user";
-  const password = process.env.PGPASSWORD ? `:${process.env.PGPASSWORD}` : "";
-  const port = process.env.PGPORT || 5432;
-  const database = process.env.PGDATABASE || "messages";
+  const connectionString =
+    process.env.DB_CONNECTION_STRING ||
+    "postgresql://username:password@localhost:5432/messages";
 
-  return {
-    connectionString: `postgresql://${username}${password}@${url}:${port}/${database}`,
-  };
+  return { connectionString };
 }
